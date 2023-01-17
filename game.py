23147d1f900e8f927ecb03aaaf8153acfaaf07c8
Luis Felipe Cajero <luis98_cajero@hotmail.com>
2023-01-17 17:58:36 -0600

    Completando los if para terminar el juego

--- game.py
+++ game.py
@@ -11,7 +11,20 @@ def run ():
         else:
             print('Papel gana piedra')
             print('La computadora ha ganado!')
-
+    elif user_option == 'papel':
+        if computer_option == 'tijera':
+            print('Tijera gana a papel')
+            print('Gana la computadora')
+        else:
+            print('Papel gana a piedra')
+            print('Has ganado!')
+    elif user_option == 'tijeras':
+        if computer_option == 'piedra':
+            print('Piedra gana tijeras')
+            print('Gana el computador')
+        else:
+            print('Tijeras gana a papel')
+            print('Has ganado!')
 
 if __name__ =="__main__":
     run()
